6e8ca3af26e9dd7cc14edc856dbfce7b0b2a5bc0
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-04-01 11:43:32 +0300

    Craete: lesson1

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-let = money;
-let = income;
+let money = 10;
+let income = 10000 ;
+let addExpenses = 2000;
+let deposit = 100000;
+let mission = 1000000000000000000;
+let period = '20 лет ';
+
+
+alert('Приветик?');
+console.log('До свидания!');
