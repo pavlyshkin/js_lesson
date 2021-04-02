603aede18002b340eba33d82751700bfe4d0054a
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-04-02 14:40:21 +0300

    Compite homework

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-let money = 10;
-let income = 10000 ;
-let addExpenses = 2000;
-let deposit = 100000;
-let mission = 1000000000000000000;
-let period = '20 лет ';
+let money = 100000;
+console.log(typeof money);
+let income = 'Фриланс';
+console.log(typeof income);
+let addExpenses = 'Еда, развлечение, такси, квартира';
+console.log(addExpenses.length);
+let deposit = true;
+console.log(typeof deposit);
+let mission = 1500000;
+let period = 6;
+let budgetDay = 150000;
+console.log(budgetDay / 30);
 
 
-alert('Приветик?');
-console.log('До свидания!');
+// alert('Приветик?');
+// console.log('До свидания!');
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split());
